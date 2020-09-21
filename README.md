c228344c92c4f883120bb96ac2c0c05c1469c491
eetiez <50176970+eetiez@users.noreply.github.com>
2020-09-21 22:25:15 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # RPI4-fan-config
-Files to configure RPI4 fan
+
+This repository contains files to configure a Raspberry Pi 4 fan. This work is based on files provided with [Argon ONE RPi4 case](https://www.argon40.com/argon-one-raspberry-pi-4-case.html).
+
+## Prerequisite
+
+I2C must be enable. For those running Archlinux ARM image, instructions are given on [this page](https://archlinuxarm.org/platforms/armv6/raspberry-pi).
+
+## How to use it ?
+
+Simply copy `fan.service` and `fan.py` at the right place and adjust permissions.
+
+```bash
+cp fan.service /lib/systemd/system/
+chmod 644 /lib/systemd/system/fan.service
+cp fan.py /usr/local/sbin/
+chmod 755 /usr/local/sbin/fan.py
+```
---
+++ fan.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import smbus
+import os
+import time
+from threading import Thread
+
+bus = smbus.SMBus(1)
+
+def get_fanspeed(tempval, configlist):
+	for curconfig in configlist:
+		curpair = curconfig.split("=")
+		tempcfg = float(curpair[0])
+		fancfg = int(float(curpair[1]))
+		if tempval >= tempcfg:
+			return fancfg
+	return 0
+
+def temp_check():
+	fanconfig = ["65=100", "60=55", "55=10"]
+	address=0x1a
+	prevblock=0
+	while True:
+		temp = os.popen("/sys/class/thermal/thermal_zone0/temp").readline()
+		val = float(temp)/1000
+		block = get_fanspeed(val, fanconfig)
+		if block < prevblock:
+			time.sleep(30)
+		prevblock = block
+		try:
+			bus.write_byte(address,block)
+		except IOError:
+			temp=""
+		time.sleep(30)
+
+try:
+	t = Thread(target = temp_check)
+	t.start()
+except:
+	t.stop()
